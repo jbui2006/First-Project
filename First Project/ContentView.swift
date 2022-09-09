44bd4d78b8ff05bb54253d7b4927484f88b899b8
jbui <jbui@paypal.com>
2022-09-09 00:49:43 -0700

    Initial Commit

---
+++ First Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First Project
+//
+//  Created by John Bui on 9/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
